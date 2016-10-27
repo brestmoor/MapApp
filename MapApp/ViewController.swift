efb2f020b0928f2b1d385d2e0923172eefa05212
UŇľytkownik GoŇõńá <Guest@imac-04.local>
2016-10-27 10:37:14 +0200

    completed

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var buttonStart: UIButton!
+    @IBOutlet var buttonStop: UIButton!
+    @IBOutlet var buttonClear: UIButton!
+    @IBOutlet var mapView: MKMapView!
 
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        buttonStop.enabled = false;
+        
+        if (CLLocationManager.locationServicesEnabled()){
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonStartClicked(sender: UIButton) {
+        buttonStart.enabled = false
+        buttonStop.enabled = true
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
     }
-
-
+    
+    @IBAction func buttonStopClicked(sender: UIButton) {
+        buttonStart.enabled = true
+        buttonStop.enabled = false
+        locationManager.stopUpdatingLocation()
+        mapView.showsUserLocation = false
+    }
+    
+    @IBAction func buttonClearClicked(sender: UIButton) {
+        mapView.removeAnnotations(mapView.annotations)
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation = locations.last?.coordinate
+        
+        let marker = MKPointAnnotation()
+        
+        marker.coordinate = currentLocation!
+        mapView.addAnnotation(marker)
+        
+        var spanDelta = 0.0
+        
+        if let speed = locations.last?.speed where speed > 0 {
+            spanDelta = (locations.last?.speed)! / 5000
+        }
+        
+        let locationArea = MKCoordinateRegion(center: currentLocation!, span: MKCoordinateSpan(latitudeDelta: spanDelta, longitudeDelta: spanDelta))
+        mapView.setRegion(locationArea, animated: true)
+    }
 }
 
